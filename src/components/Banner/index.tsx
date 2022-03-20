3c69d047f3058809f0107e973efb89cafaa87945
sambuite <murilosambuite@gmail.com>
2022-03-20 08:30:50 -0300

    feat: create Banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,33 @@
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
+
+export function Banner() {
+  return (
+    <Flex
+      w="100vw"
+      h="80"
+      align="center"
+      justify="center"
+      bgImage="url('https://images.unsplash.com/photo-1495344517868-8ebaf0a2044a')"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+    >
+      <Flex
+        w="100%"
+        maxW={1240}
+        h="100%"
+        align="center"
+        justifyContent="space-between"
+      >
+        <Box width="35%">
+          <Heading whiteSpace="pre" color="gray.200">
+            {'5 Continents,\ninfinitas possibilidades'}
+          </Heading>
+          <Text color="gray.500" mt="5">
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+          </Text>
+        </Box>
+        <Image src="/airplane.svg" alt="avião" w="96" mb="-32" />
+      </Flex>
+    </Flex>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Flex } from '@chakra-ui/react';
+import { Banner } from '../components/Banner';
 import { Header } from '../components/Header';
 
 export default function Home() {
   return (
     <Flex direction="column" h="100vh" w="100wh">
       <Header />
+      <Banner />
     </Flex>
   );
 }
