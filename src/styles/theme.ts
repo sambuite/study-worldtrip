02956342582bd7651fdf76960cacf9710079baf3
sambuite <murilosambuite@gmail.com>
2022-03-21 09:27:16 -0300

    fix: fix extended theme color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,18 +2,19 @@ import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
   colors: {
-    primary: {
+    yellow: {
       '500': '#FFBA08',
     },
   },
   fonts: {
     heading: 'Poppins',
     body: 'Poppins',
+    mono: 'Barlow',
   },
   styles: {
     global: {
       body: {
-        bg: 'white',
+        bg: 'gray.50',
         color: 'gray.600',
       },
     },
